85e0b352acb963f663d7fbf2468b3d4a6618bccf
Adrian <radu.p1995@yahoo.com>
2024-05-11 16:31:44 +0300

    solved categorie filter error and added the query by firestore

--- app/(admin-clinica)/verificare-tranzactie/[id]/page.jsx
+++ app/(admin-clinica)/verificare-tranzactie/[id]/page.jsx
@@ -1,25 +1,16 @@
-"use client";
-
-import Header from "@/components/common/header/dashboard/Header";
-import SidebarMenu from "@/components/common/header/dashboard/SidebarMenu";
-import MobileMenu from "@/components/common/header/MobileMenu";
-import CreateList from "@/components/dashboard/verificare-tranzactie/CreateList";
-import DetailedInfo from "@/components/dashboard/creaza-oferta/DetailedInfo";
-import FloorPlans from "@/components/dashboard/creaza-oferta/FloorPlans";
-import LocationField from "@/components/dashboard/creaza-oferta/LocationField";
-import PropertyMediaUploader from "@/components/dashboard/creaza-oferta/PropertyMediaUploader";
+import dynamic from "next/dynamic";
 import {
   handleQueryFirestore,
   handleQueryFirestoreSubcollection,
 } from "@/utils/firestoreUtils";
-import { authentication } from "@/firebase";
-import { verifyCurrentUser } from "@/utils/commonUtils";
-import Form from "@/components/autentificare-partener/Form";
-import TransactionVerification from "@/components/dashboard/verificare-tranzactie/TransactionVerification";
-import { useAuth } from "@/context/AuthContext";
-import { useEffect, useState } from "react";
+import Tranzactie from "@/components/dashboard/verifica-tranzactie/Tranzactie";
 
-const index = ({ params }) => {
+export const metadata = {
+  title: "Verifica tranzactie || ExclusivMD",
+  description: "ExclusivMD",
+};
+
+const index = async ({ params }) => {
   const { id } = params;
   const parts = id.split("-");
   const userId = parseFloat(parts[0]);
@@ -28,114 +19,21 @@ const index = ({ params }) => {
   const offerId = codParts[0];
   const partenerId = codParts[1];
 
-  const { userData, loading } = useAuth();
-  const [isVerified, setIsVerified] = useState(false);
-  const [utilizator, setUtilizator] = useState([]);
-  const [oferta, setOferta] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const utilizatorData = await handleQueryFirestore("Users", "id", userId);
-      setUtilizator(utilizatorData);
-
-      const ofertaData = await handleQueryFirestoreSubcollection(
-        "Oferte",
-        "documentId",
-        offerId
-      );
-      setOferta(ofertaData);
-    };
-
-    fetchData();
-  }, [userId, offerId]);
-
-  useEffect(() => {
-    const verifyCurrentUser = () => {
-      if (partenerId) {
-        console.log("is partener id...");
-        if (
-          (!loading && userData?.userType !== "Partener") ||
-          (!loading && userData.user_uid !== partenerId)
-        ) {
-          console.log("not first...");
-          setIsVerified(false);
-        } else {
-          console.log("is first...");
-          setIsVerified(true);
-        }
-      } else {
-        if (!loading && userData?.userType !== "Partener") {
-          console.log("not second...");
-          setIsVerified(false);
-        } else {
-          console.log("is second...");
-          setIsVerified(true);
-        }
-      }
-    };
-
-    verifyCurrentUser();
-  }, [partenerId, userData, loading]);
-
+  const utilizatorData = await handleQueryFirestore("Users", "id", userId);
+  const ofertaData = await handleQueryFirestoreSubcollection(
+    "Oferte",
+    "documentId",
+    offerId
+  );
   return (
     <>
-      {/* <!-- Main Header Nav --> */}
-      {!isVerified && <Header />}
-
-      {/* <!--  Mobile Menu --> */}
-      {!isVerified && <MobileMenu />}
-
-      {!isVerified && (
-        <div className="dashboard_sidebar_menu">
-          <div
-            className="offcanvas offcanvas-dashboard offcanvas-start"
-            tabIndex="-1"
-            id="DashboardOffcanvasMenu"
-            data-bs-scroll="true"
-          >
-            <SidebarMenu partenerId={partenerId} />
-          </div>
-        </div>
-      )}
-      {/* End sidebar_menu */}
-
-      {/* <!-- Our Dashbord --> */}
-      <section className="our-dashbord dashbord bgc-f7 pb50">
-        <div className="container-fluid ovh">
-          <div className="row">
-            <div className="col-lg-12 maxw100flex-992">
-              <div className="row">
-                {/* Start Dashboard Navigation */}
-                <div className="col-lg-12">
-                  <div className="dashboard_navigationbar dn db-1024">
-                    <div className="dropdown">
-                      <button
-                        className="dropbtn"
-                        data-bs-toggle="offcanvas"
-                        data-bs-target="#DashboardOffcanvasMenu"
-                        aria-controls="DashboardOffcanvasMenu"
-                      >
-                        <i className="fa fa-bars pr10"></i> Navigatie cont
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                {/* End Dashboard Navigation */}
-
-                <TransactionVerification
-                  isVerified={isVerified}
-                  utilizator={utilizator[0] || []}
-                  oferta={oferta[0] || []}
-                />
-              </div>
-              {/* End .row */}
-            </div>
-            {/* End .col */}
-          </div>
-        </div>
-      </section>
+      <Tranzactie
+        partenerId={partenerId}
+        utilizator={utilizatorData[0]}
+        oferta={ofertaData[0]}
+      />
     </>
   );
 };
 
-export default index;
+export default dynamic(() => Promise.resolve(index), { ssr: false });
