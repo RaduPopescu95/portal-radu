2cf9e18abf70ba420347185c32b57594d347ea5f
Adrian <radu.p1995@yahoo.com>
2024-06-05 19:47:43 +0300

    modificari realizate

--- components/common/CallToAction.jsx
+++ components/common/CallToAction.jsx
@@ -6,10 +6,15 @@ const CallToAction = () => {
       <div className="col-lg-8">
         <div className="start_partner tac-smd">
           <h2>Devino partenerul nostru</h2>
-          <p>Alătură-te rețelei noastre de elită și accesează noi oportunități de creștere. Împreună, putem oferi pacienților acces la servicii medicale superioare și deschide drumul spre succesul clinicii tale. Contactează-ne pentru a începe!</p>      </div>
-      {/* End .col */}
+          <p>
+            Alătură-te rețelei noastre de elită și accesează noi oportunități de
+            creștere. Împreună, putem oferi cadrelor medicale acces exclusiv la
+            produse și servicii de calitate și oferte competitive.
+            Contactează-ne pentru a începe!
+          </p>{" "}
+        </div>
+        {/* End .col */}
       </div>
-
       <div className="col-lg-4">
         <div className="parner_reg_btn text-right tac-smd">
           <Link href="/contact" className="btn btn-thm2">
